ed256a210e9a747b9ef0a4575d7b9626452de285
Yeuh-Hua Tu <a504082002@gmail.com>
2016-08-14 02:45:21 +0800

    fix bug

--- batch.py
+++ batch.py
@@ -7,7 +7,7 @@ cmds = map(lambda x: x.replace("'", ""), sys.argv[2:])
 
 pool = multiprocessing.Pool(proc_num)
 for cmd in cmds:
-    pool.apply(os.system, args=cmd)
+    pool.apply(os.system, args=(cmd, ))
 pool.close()
 pool.join()
 
